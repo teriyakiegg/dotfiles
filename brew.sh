4fa00f87852f7ef1c6cf8b0bf26790d9e03f538f
teriyakiegg <teriyakiegg@gmail.com>
2019-10-20 21:29:01 +0900

    Create brew.sh

---
+++ brew.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Install useful binaries.
+brew install git
+brew install tig
+brew install peco
+brew install hub
+
+# Remove outdated versions from the cellar.
+brew cleanup
